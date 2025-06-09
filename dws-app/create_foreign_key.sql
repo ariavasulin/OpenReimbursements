99025b0b4ba758afd9b42b71fc76bb226583f83e
ariavasulin <ari@ariavasulin.com>
2025-06-09 12:12:57 +0200

    OCr Upgrade Plan

--- dws-app/create_foreign_key.sql
+++
@@ -1,5 +0,0 @@
--- Create foreign key relationship between receipts and user_profiles
-ALTER TABLE receipts 
-ADD CONSTRAINT receipts_user_id_fkey 
-FOREIGN KEY (user_id) 
-REFERENCES user_profiles(user_id); 
